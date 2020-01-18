5b86580f9bcaab08ee05dbdbbbb5c26852ae7bfc
Bartlomiej Wierzbicki <bartlomiejw96@gmail.com>
2020-01-18 13:42:14 +0100

    Init commit

---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Form = props => {
+  return (
+    <form className="App" onSubmit={props.onSubmit}>
+      <label>
+        Amount:{" "}
+        <input type="number" value={props.amount} onChange={props.onChange} />{" "}
+        PLN
+      </label>
+      <br></br>
+      <label>
+        Select category:{" "}
+        <select onChange={props.onChangeCategory}>
+          {["Food", "Home", "Other", "Salary"].map((item, index) => (
+            <option key={index} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <br></br>
+        <label>
+          Date:{" "}
+          <input
+            onChange={props.onChangeDate}
+            value={props.date}
+            type="date"
+            required={true}
+          ></input>
+        </label>
+      </label>
+      <br></br>
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default Form;
