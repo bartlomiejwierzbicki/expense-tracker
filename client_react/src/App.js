463b5f10edeb97ee749431aa26338ec50dc444c9
Bartek <bartlomiejw96@gmail.com>
2020-03-18 18:43:04 +0100

    Change style

---
+++ client_react/src/App.js
@@ -0,0 +1,149 @@
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import RoundChart from "./components/RoundChart";
+import LineChart from "./components/LineChart";
+import TableData from "./components/TableData";
+import Navigation from "./components/Navigation";
+
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      amount: "",
+      category: "Food",
+      categoryIncome: "Salary",
+      months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ],
+      expenses: [],
+      incomes: [],
+      expenseCategory: ["Food", "Home", "Transport", "Health", "Fun", "Other"],
+      incomeCategory: ["Salary", "Gift", "Other"],
+      expensesColumn: [
+        {
+          title: "Date",
+          field: "date",
+          type: "date",
+          initialEditValue: new Date()
+        },
+        {
+          title: "Category",
+          field: "category",
+          lookup: {
+            Food: "Food",
+            Home: "Home",
+            Transport: "Transport",
+            Health: "Health",
+            Fun: "Fun",
+            Other: "Other"
+          },
+          initialEditValue: "Food"
+        },
+        { field: "amount", type: "currency" }
+      ],
+      incomesColumn: [
+        { title: "Date", field: "date", type: "date" },
+        {
+          title: "Category",
+          field: "category",
+          lookup: { Salary: "Salary", Gift: "Gift", Other: "Other" }
+        },
+        { field: "amount", type: "currency" }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    axios
+      .all([
+        axios.get("http://localhost:5000/api/expense"),
+        axios.get("http://localhost:5000/api/income")
+      ])
+      .then(
+        axios.spread((...responses) => {
+          this.setState({
+            expenses: responses[0].data,
+            incomes: responses[1].data
+          });
+        })
+      )
+      .catch(errors => {
+        console.log("Error");
+      });
+  };
+
+  render() {
+    return (
+      <Router>
+        <div style={{ display: "flex" }}>
+          <Navigation></Navigation>
+          <Container style={{ marginTop: "10vh" }} fixed>
+            <Switch>
+              <Route path="/expenses">
+                <TableData
+                  type={"expense"}
+                  fetchData={this.fetchData}
+                  columns={this.state.expensesColumn}
+                  data={this.state.expenses}
+                  title={"Expenses"}
+                />
+              </Route>
+              <Route path="/incomes">
+                <TableData
+                  type={"income"}
+                  fetchData={this.fetchData}
+                  columns={this.state.incomesColumn}
+                  data={this.state.incomes}
+                  title={"Incomes"}
+                />
+              </Route>
+
+              <Route exact path="/">
+                <Grid container spacing={5}>
+                  <Grid item xs sm={12} lg={6}>
+                    <Paper elevation={2} style={{ padding: "5%" }}>
+                      <RoundChart
+                        data={this.state.expenses}
+                        categorySets={this.state.expenseCategory}
+                      />
+                    </Paper>
+                  </Grid>
+                  <Grid item xs sm={12} lg={6}>
+                    <Paper elevation={2} style={{ padding: "5%" }}>
+                      <LineChart
+                        months={this.state.months}
+                        data={this.state.expenses}
+                        incomes={this.state.incomes}
+                      />
+                    </Paper>
+                  </Grid>
+                </Grid>
+              </Route>
+            </Switch>
+          </Container>
+        </div>
+      </Router>
+    );
+  }
+}
--- client_react/src/components/Navigation.js
+++ client_react/src/components/Navigation.js
@@ -1,30 +1,43 @@
 import React from "react";
 import clsx from "clsx";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import { NavLink } from "react-router-dom";
+import DashboardIcon from "@material-ui/icons/Dashboard";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import DashboardIcon from "@material-ui/icons/Dashboard";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
-import { NavLink } from "react-router-dom";
-
-const drawerWidth = 220;
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
@@ -42,56 +55,61 @@ const useStyles = makeStyles(theme => ({
     })
   },
   menuButton: {
-    marginRight: 16
+    marginRight: 36
   },
-  hide: {
+  menuButtonHidden: {
     display: "none"
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: "nowrap"
+  title: {
+    flexGrow: 1
   },
-  drawerOpen: {
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
     width: drawerWidth,
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen
     })
   },
-  drawerClose: {
+  drawerPaperClose: {
+    overflowX: "hidden",
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
     }),
-    overflowX: "hidden",
-    width: theme.spacing(7) + 1,
+    width: theme.spacing(7),
     [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9) + 1
+      width: theme.spacing(9)
     }
   },
-  toolbar: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: theme.spacing(0, 0),
-    ...theme.mixins.toolbar
-  },
+  appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
-    padding: theme.spacing(1)
+    height: "100vh",
+    overflow: "auto"
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column"
+  },
+  fixedHeight: {
+    height: 240
   }
 }));
 
 export default function Navigation() {
   const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
+  const [open, setOpen] = React.useState(true);
   const handleDrawerOpen = () => {
     setOpen(true);
   };
-
   const handleDrawerClose = () => {
     setOpen(false);
   };
@@ -100,59 +118,70 @@ export default function Navigation() {
     <div className={classes.root}>
       <CssBaseline />
       <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open
-        })}
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
       >
-        <Toolbar>
+        <Toolbar className={classes.toolbar}>
           <IconButton
+            edge="start"
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open
-            })}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
           >
             <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            Expense Tracker
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton color="inherit">
+            <Badge color="secondary">
+              <AccountCircleIcon></AccountCircleIcon>
+            </Badge>
           </IconButton>
         </Toolbar>
       </AppBar>
       <Drawer
         variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open
-        })}
         classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open
-          })
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose)
         }}
+        open={open}
       >
-        <div className={classes.toolbar}>
+        <div className={classes.toolbarIcon}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-                <ChevronLeftIcon />
-              )}
+            <ChevronLeftIcon />
           </IconButton>
         </div>
         <Divider />
-        <NavLink to="/" style={{ textDecoration: "none", color:"black" }}>
+        <NavLink to="/" style={{ textDecoration: "none", color: "black" }}>
           <List>
             <ListItem button key={1}>
               <ListItemIcon>
                 <DashboardIcon />
               </ListItemIcon>
-              <ListItemText primary={"Home"} />
+              <ListItemText primary={"Dashboard"} />
             </ListItem>
           </List>
         </NavLink>
-        <NavLink to="/expenses" style={{ textDecoration: "none", color:"black" }}>
+        <NavLink
+          to="/expenses"
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <List>
             <ListItem button key={2}>
               <ListItemIcon>
@@ -162,7 +191,10 @@ export default function Navigation() {
             </ListItem>
           </List>
         </NavLink>
-        <NavLink to="/incomes" style={{ textDecoration: "none", color:"black" }}>
+        <NavLink
+          to="/incomes"
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <List>
             <ListItem button key={3}>
               <ListItemIcon>
@@ -172,8 +204,23 @@ export default function Navigation() {
             </ListItem>
           </List>
         </NavLink>
+        <NavLink
+          to="/reports"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <List>
+            <ListItem button key={3}>
+              <ListItemIcon>
+                <BarChartIcon></BarChartIcon>
+              </ListItemIcon>
+              <ListItemText primary={"Reports"} />
+            </ListItem>
+          </List>
+        </NavLink>
       </Drawer>
-      <div className={classes.toolbar} />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+      </main>
     </div>
   );
 }
